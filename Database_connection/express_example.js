1fd11b6d747a8e3ddd8d10e118ff7f30dd1ce3d2
RedinOmar <redinomar06@gmail.com>
2022-09-13 14:49:47 +0200

    express_example.js

---
+++ Database_connection/express_example.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = 3000
+var mysql = require('mysql');
+
+app.get('/', (req, res) => {
+  // serveren skal respondere til databasen
+  connectDatabase("select * from elev");
+  console.log(mysql);
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+function connectDatabase(createConnection) {
+
+  var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "Dromtorp"
+  });
+
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+  module.exports = con;
+};
