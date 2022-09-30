6c1104fc85b47d3249f7207410dc8f894b0107d7
RedinOmar <redinomar06@gmail.com>
2022-09-30 12:21:47 +0200

    1

--- ExpressAPI/frontend/JS/get_request.js
+++ ExpressAPI/frontend/JS/get_request.js
@@ -1,11 +1,11 @@
-function getRequest() {
+/*function getRequest() {
   console.log('Funksjonen fungerer');
   var inputText = document.getElementById('fname').value;
   console.log(inputText);
   //var sql = "SELECT * FROM elev WHERE Fornavn="+inputText+''
   //console.log(sql);
+}*/
 
-  fetch('http://localhost:3000/')
-    .then((response) => response.json())
-    .then((data) => console.log(data));
-}
+fetch('http://localhost:3000/')
+  .then((response) => response.json())
+  .then((data) => console.log(data));
