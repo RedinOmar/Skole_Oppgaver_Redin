b22c20819e90232c0f53c19c10cea56ee07ff25c
RedinOmar <redinomar06@gmail.com>
2022-12-01 14:49:07 +0100

    Redin

--- TO_Prosjekt/SPILL/JS/script.js
+++ TO_Prosjekt/SPILL/JS/script.js
@@ -8,33 +8,33 @@ let questions = {
   "answers": [ "Frøplanter", "Virveldyr", "Krypdyr", "Pattedyr" ],
   "correctIndex": 3 },
 
-  { "question": "Who are the actors in The Internship?",
-  "answers": [ "Ben Stiller, Jonah Hill", "Courteney Cox, Matt LeBlanc", "Kaley Cuoco, Jim Parsons", "Vince Vaughn, Owen Wilson" ],
-  "correctIndex": 3 },
-
-  { "question": "What is the capital of Spain?",
-  "answers": [ "Berlin", "Buenos Aires", "Madrid", "San Juan" ],
+  { "question": "Hva er forventet levealderen til Hester?",
+  "answers": [ " 19-27, 42-50, 25-30, 31-35" ],
   "correctIndex": 2 },
 
-  { "question": "What are the school colors of the University of Texas at Austin?",
-  "answers": [ "Black, Red", "Blue, Orange", "White, Burnt Orange", "White, Old gold, Gold" ],
-  "correctIndex": 2 },
-
-  { "question": "What is 70 degrees Fahrenheit in Celsius?",
-  "answers": [ "18.8889", "20", "21.1111", "158" ],
-  "correctIndex": 2 },
-
-  { "question": "When was Mahatma Gandhi born?",
-  "answers": [ "October 2, 1869", "December 15, 1872", "July 18, 1918", "January 15, 1929" ],
+  { "question": "I 2010, satt en hest ny rekord på å være den høyeste. Hvor høy var hesten?",
+  "answers": [ "2,1 Meter høy", "1,8 Meter høy", "1,9 Meter høy", "2,0 Meter høy" ],
   "correctIndex": 0 },
 
-  { "question": "How far is the moon from Earth?",
-  "answers": [ "7,918 miles (12,742 km)", "86,881 miles (139,822 km)", "238,400 miles (384,400 km)", "35,980,000 miles (57,910,000 km)" ],
+  { "question": "hvor mye kan hester veie i gjennomsnitt?",
+  "answers": [ "300-1000 Kg", "400-900 Kg", "620-850 Kg", "430-760 Kg" ],
+  "correctIndex": 1 },
+
+  { "question": "Hvor kjapt kan hester løpe maks?",
+  "answers": [ "92 Km/t", "100 Km/t", "88 Km/t", "76 Km/t" ],
   "correctIndex": 2 },
 
-  { "question": "What is 65 times 52?",
-  "answers": [ "117", "3120", "3380", "3520" ],
-  "correctIndex": 2 },
+  { "question": "Hva spiser hester vanligvis?",
+  "answers": [ "gresskar", "gullerot", "eple", "Gress"],
+  "correctIndex": 3 },
+
+  { "question": "Hva er favoritt maten til hester?",
+  "answers": [ "Eple og gullerot", "Banan og durer", "Gresskar og meloner", "Jordbær og bringbær" ],
+  "correctIndex": 0 },
+
+  { "question": "Hvor mye kan hester se rundt seg?",
+  "answers": [ "Mer enn 90 grader", " Mer enn 180 grader", "270 grader", "Nesten 360 grader" ],
+  "correctIndex": 3 },
 
   { "question": "How tall is Mount Everest?",
   "answers": [ "6,683 ft (2,037 m)", "7,918 ft (2,413 m)", "19,341 ft (5,895 m)", "29,029 ft (8,847 m)" ],
@@ -89,21 +89,15 @@ function answer(button) {
     document.getElementById('svar_2').style.display = "none";
     document.getElementById('svar_3').style.display = "none";
     document.getElementById('svar_4').style.display = "none";
-
     document.getElementById('end-game').style.display = "";
     document.getElementById('end-game').style.display = "flex";
-
-    document.getElementById('try_again').style.visibility = "visible";
-    document.getElementById('stop_game').style.visibility = "visible";
   }
 }
 
 function try_again() {
   display_highscore = 0;
   quiz = 0;
-  document.getElementById('try_again').style.display = "none";
-  document.getElementById('stop_game').style.display = "none";
-
+  document.getElementById('end-game').style.display = "none";
   document.getElementById('question_title').style.display = "";
   document.getElementById('theme').style.display = "";
   document.getElementById('svar_1').style.display = "";
