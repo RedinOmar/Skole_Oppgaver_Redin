99607b83b8c494d87f811b32545bb7f8a7f2c02a
Redin Omar <redinomar1@gmail.com>
2023-01-05 14:41:21 +0100

    redin

--- OBJ_Programming/JS/javascript.js
+++ OBJ_Programming/JS/javascript.js
@@ -5,7 +5,6 @@ class klassenavn {
     this.klassetrinn = klassetrinn;
   }
 };
-
 //lage en instans av klassen - oppreter en klasse
 let i2ta = new klassenavn('Informasjonsteknologi', 'vg2');
 console.log(i2ta);
@@ -37,6 +36,7 @@ console.log('omkrets:', areal.toFixed(2));
 
 
 
+// sylinder
 class sylinder extends sirkel{
   constructor(radius, høyde) {
     super(radius);
@@ -51,15 +51,43 @@ let volum_av_sylinder = sylinderinstanse.finnvolum();
 console.log(volum_av_sylinder);
 
 
+
+
+
+// Oppgave 4.03 a, b, c
 class dinalder {
   constructor(navn, fødselsår, kalender) {
-    this.navn = "Ola";
-    this.fødselsår = 2000;
+    this.navn = navn;
+    this.fødselsår = fødselsår;
     this.kalender = kalender;
   }
   finnalder() {
-    return
+    return Math.abs(this.kalender - this.fødselsår);
   }
 }
-let person = new dinalder('Ola', 2000, '');
-console.log(person);
+let dinalderValue = new dinalder("Ola", 2000, 2022);
+let alder = dinalderValue.finnalder();
+console.log(alder);
+
+
+
+
+
+class inputAlder {
+  constructor(nameValue, fødselsårValue, kalenderValue) {
+    this.nameValue = nameValue;
+    this.fødselsårValue = fødselsårValue;
+    this.kalenderValue = kalenderValue;
+  }
+  calculateAge(nameInput, ageInput) {
+    nameInput = document.getElementById('name').value;
+    ageInput = document.getElementById('age').value;
+    this.nameValue = nameInput;
+    this.fødselsårValue = ageInput;
+    return Math.abs(this.nameValue - this.fødselsårValue);
+    document.getElementById('nameAndage').innerHTML = age.nameValue + ageValue;
+  }
+}
+let age = new inputAlder(this.nameValue, this.fødselsårValue, 2022);
+let ageValue = age.calculateAge();
+console.log(ageValue);
