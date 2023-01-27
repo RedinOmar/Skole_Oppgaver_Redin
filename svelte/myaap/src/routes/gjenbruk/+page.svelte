cb844d6d1c07bf1a1fe774279bb7169f6ef78f57
Redin Omar <redinomar1@gmail.com>
2023-01-27 14:18:49 +0100

    reidn

---
+++ svelte/myaap/src/routes/gjenbruk/+page.svelte
@@ -0,0 +1,18 @@
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p id="p"></p>
+    <script>
+        import {PI, Eulertallet, lysfart, avogadrokonstant, bohrkonstant, radius} from "./konstanter.js";
+
+        export let areal () {
+            let sirkel = PI * radius * radius;
+            return sirkel;
+        }
+        console.log({areal});
+    </script>    
+</body>
+</html>
---
+++ svelte/myaap/src/routes/gjenbruk/konstanter.js
@@ -0,0 +1,8 @@
+let PI = 3.14159;
+let Eulertallet = 2.17828;
+let lysfart = 3.00 * 10^8;
+let avogadrokonstant = 6.02 * 10^-23;
+let bohrkonstant = 2.18 * 10^-18;
+let radius = 2;
+
+export {PI, Eulertallet, lysfart, avogadrokonstant, bohrkonstant, radius};
