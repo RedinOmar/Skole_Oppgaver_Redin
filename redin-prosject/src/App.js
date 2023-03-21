2fd7eff46c9f4e185ad913df9afa039acf7e2e92
Redin Omar <redinomar1@gmail.com>
2023-03-21 14:39:43 +0100

    redin

--- redin-prosject/src/App.js
+++ redin-prosject/src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Er du...</h1>
+
+        <div className='confirm-box'>
+          <div className='confirm-user'>
+            <h2>En Bruker</h2>
+            <button onClick={() => alert('cliked')}>Login</button>
+            <button onClick={() => alert('cliked')}>Sign In</button>
+          </div>
+
+          <h2>Eller</h2>
+
+          <div className='confirm-employe'>
+            <h2>En Ansatt</h2>
+            <button onClick={() => alert('cliked')}>Login</button>
+          </div>
+        </div>
       </header>
     </div>
   );
